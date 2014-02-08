34b3ea2af5951f2d2e181b1090a32a9f44fd3069
Andrew Duthie <aduthie7@gmail.com>
2014-02-07 20:13:02 -0500

    Add progress routing and route middleware support

--- api/config/routes.js
+++ api/config/routes.js
@@ -1,14 +1,21 @@
 module.exports = function (app) {
   restify(app, 'regimen');
   restify(app, 'user');
+  restify(app, 'progress')
 };
 
 var restify = function(app, base) {
-  var router = require('../routers/' + base);
-  app.get('/' + base + '/', router.index);
-  app.get('/' + base + '/:id', router.show);
-  app.post('/' + base + '/', router.create);
-  app.put('/' + base + '/:id', router.update);
-  app.patch('/' + base + '/:id', router.patch);
-  app.del('/' + base + '/:id', router.delete);
+  var router = require('../routers/' + base),
+    middleware = router.middleware || passthrough;
+
+  app.get('/' + base + '/', middleware, router.index);
+  app.get('/' + base + '/:id', middleware, router.show);
+  app.post('/' + base + '/', middleware, router.create);
+  app.put('/' + base + '/:id', middleware, router.update);
+  app.patch('/' + base + '/:id', middleware, router.patch);
+  app.del('/' + base + '/:id', middleware, router.delete);
+};
+
+var passthrough = function(req, res, next) {
+  next();
 };
---
+++ api/routers/progress.js
@@ -0,0 +1,48 @@
+var Promise = require('bluebird'),
+  Regimen = Promise.promisifyAll(require('../models/regimen.js'));
+
+Promise.promisifyAll(Regimen.prototype);
+
+exports.middleware = [
+  function(req, res, next) {
+    // Verify regimenId passed as query parameter
+    if (!req.query.regimenId) {
+      res.status(400);
+
+      var error = 'Invalid or missing `regimenId` query parameter.';
+      res.send({ error: error });
+      return next(error);
+    }
+
+    next();
+  }
+];
+
+// List
+exports.index = function(req, res) {
+  var regimenId = req.query.regimenId;
+
+  Regimen.findOneAsync({ _user: req.user.id, _id: regimenId }).then(function(regimen) {
+    // Respond with progress property
+    res.send(regimen.progress);
+  });
+};
+
+// Create
+exports.create = function(req, res) {
+  var regimenId = req.query.regimenId;
+
+  Regimen.findOneAsync({ _user: req.user.id, _id: regimenId }).then(function(regimen) {
+    regimen.progress.push(req.body);
+    return regimen.saveAsync();
+  }).then(function(regimen) {
+    res.status(201); // 201: Created
+    res.end();
+  });
+};
+
+// Undefined: Show, Update, Patch, Delete
+exports.show = exports.update = exports.patch = exports.delete = function(req, res) {
+  res.status(404);
+  res.end();
+};
